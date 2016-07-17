bcf112f976635b781aa9262eb70882d19aa30ff3
Brett Wise <brettdwise@gmail.com>
2016-07-17 14:05:14 -0500

    First pass at solution w/ tests

--- 1_tip_calc/test/tip_calc_test.exs
+++ 1_tip_calc/test/tip_calc_test.exs
@@ -5,4 +5,16 @@ defmodule TipCalcTest do
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  test "find tip amount" do
+    assert TipCalc.calc_tip(20, 25.53) == 5.11
+  end
+
+  test "test if decimal tips are just used in calculation instead of being divided" do
+    assert TipCalc.calc_tip(0.15, 25.53) == 3.83
+  end
+
+  test "test if total is calculated properly" do
+    assert TipCalc.calc_total(3.83, 25.53) == 29.36
+  end
 end
