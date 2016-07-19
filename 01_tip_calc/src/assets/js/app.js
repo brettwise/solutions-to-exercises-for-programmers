dccc63376045bd4ba7218944592cb5edc9f73dc5
Brett Wise <brettdwise@gmail.com>
2016-07-19 00:15:58 -0400

    Basic calculator working w/o error checking or other fancy stuff

--- 01_tip_calc/src/assets/js/app.js
+++ 01_tip_calc/src/assets/js/app.js
@@ -1,1 +1,24 @@
 $(document).foundation();
+
+(function () {
+  var calcBillButton = document.getElementById('calc-bill')
+  calcBillButton.addEventListener('click', calcTotalWithTip, false);
+
+  function calcTotalWithTip() {
+
+    var tipPercentage = parseInt(
+      document.getElementById('tip-percentage').value
+      , 10
+    );
+
+    var billAmount = parseFloat(
+      document.getElementById('bill-amount').value
+    );
+
+    var billTotal = document.getElementById('bill-total').innerHTML;
+
+    var tipAmount = tipPercentage/100 * billAmount;
+    billTotal = document.getElementById('bill-total').innerHTML = tipAmount + billAmount;
+  }
+
+})();
