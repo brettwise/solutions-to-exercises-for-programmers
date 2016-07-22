a44ced5e998c1f503ae4043f44d69da01f02a256
Brett Wise <brettdwise@gmail.com>
2016-07-22 12:40:48 -0400

    Refactored to work from the command line

--- elixir/01_tip_calc/lib/tip_calc.ex
+++ elixir/01_tip_calc/lib/tip_calc.ex
@@ -1,17 +1,28 @@
 defmodule TipCalc do
+  def main(argv) do
+    {[bill_amount: bill_amount, tip_percentage: tip_percentage], _, _} = OptionParser.parse(argv,
+      switches: [bill_amount: :float, tip_percentage: :integer],
+      aliases: [ba: :bill_amount, tp: :tip_percentage] # makes '-q' an alias of '--question'
+      )
 
-  def calc_tip(tip_percentage, bill) when tip_percentage > 1 do
-    tip_percentage / 100 * bill
+      IO.puts "So your bill is: #{bill_amount}"
+      IO.puts "And you're tipping out at #{tip_percentage}%"
+      IO.puts "which is: $#{calc_tip(tip_percentage, bill_amount)}"
+      IO.puts "for a total of: $#{calc_total(tip_percentage, bill_amount)}"
+  end
+
+  def calc_tip(tip_percentage, bill_amount) when tip_percentage > 1 do
+    tip_percentage / 100 * bill_amount
     |> Float.round(2)
   end
 
-  def calc_tip(tip_percentage, bill) do
-    tip_percentage * bill
+  def calc_tip(tip_percentage, bill_amount) do
+    tip_percentage * bill_amount
     |> Float.round(2)
   end
 
-  def calc_total(tip_amount, bill) do
-    tip_amount + bill
+  def calc_total(tip_percentage, bill_amount) do
+    calc_tip(tip_percentage, bill_amount) + bill_amount
     |> Float.round(2)
   end
 
--- elixir/01_tip_calc/test/tip_calc_test.exs
+++ elixir/01_tip_calc/test/tip_calc_test.exs
@@ -10,11 +10,7 @@ defmodule TipCalcTest do
     assert TipCalc.calc_tip(20, 25.53) == 5.11
   end
 
-  test "test if decimal tips are just used in calculation instead of being divided" do
+  test "if decimal tips are just used in calculation instead of being divided" do
     assert TipCalc.calc_tip(0.15, 25.53) == 3.83
-  end
-
-  test "test if total is calculated properly" do
-    assert TipCalc.calc_total(3.83, 25.53) == 29.36
   end
 end
