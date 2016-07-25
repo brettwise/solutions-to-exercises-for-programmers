337fb5e479b7cd9f2582cf0662c73ab2ccbf870c
Brett Wise <brettdwise@gmail.com>
2016-07-25 13:30:14 -0400

    Total refactor of the code. Added doctests. Added basic error checking for negative numbers

--- elixir/01_tip_calc/lib/tip_calc.ex
+++ elixir/01_tip_calc/lib/tip_calc.ex
@@ -5,25 +5,64 @@ defmodule TipCalc do
       aliases: [ba: :bill_amount, tp: :tip_percentage] # makes '-q' an alias of '--question'
       )
 
-      IO.puts "So your bill is: #{bill_amount}"
-      IO.puts "And you're tipping out at #{tip_percentage}%"
-      IO.puts "which is: $#{calc_tip(tip_percentage, bill_amount)}"
-      IO.puts "for a total of: $#{calc_total(tip_percentage, bill_amount)}"
+    tip_float = parse_number(tip_percentage) / 100
+    bill_amount = parse_number(bill_amount)
+    tip_amount = calc_tip(tip_float, bill_amount)
+    bill_total = calc_total(tip_amount, bill_amount)
+    print_to_CLI(bill_amount, tip_percentage, tip_amount, bill_total)
   end
 
-  def calc_tip(tip_percentage, bill_amount) when tip_percentage > 1 do
-    tip_percentage / 100 * bill_amount
-    |> Float.round(2)
+  @doc """
+  Takes a number in and checks if it's positive. If it is it's floated. If already a float it's returned.
+
+      iex> TipCalc.parse_number(15)
+      15.0
+
+      iex> TipCalc.parse_number(-1)
+      "You'll have to enter a positive number for this to work."
+      :ok
+
+  """
+  def parse_number(number) do
+    case number do
+      number when number < 0 ->
+        IO.puts "You'll have to enter a positive number for this to work."
+      number when is_integer(number) ->
+        number / 1
+      number when is_float(number) ->
+        number
+    end
   end
 
+  @doc """
+  Calculates the tip amount. Takes two floats and returns a rounded number.
+
+      iex> TipCalc.calc_tip(0.15, 55.12)
+      8.27
+  """
   def calc_tip(tip_percentage, bill_amount) do
     tip_percentage * bill_amount
     |> Float.round(2)
   end
 
-  def calc_total(tip_percentage, bill_amount) do
-    calc_tip(tip_percentage, bill_amount) + bill_amount
+  @doc """
+  Calculates the total bill amount. Takes floats.
+
+      iex> TipCalc.calc_total(2.0, 4.32)
+      6.32
+  """
+  def calc_total(tip_amount, bill_amount) do
+    tip_amount + bill_amount
     |> Float.round(2)
   end
 
+  @doc """
+  Prints 4 values to the command line: the bill amount, tip percentage, tip amount, and total bill amount.
+  """
+  def print_to_CLI(bill_amount, tip_percentage, tip_amount, bill_total)  do
+    IO.puts "So your bill is: $#{bill_amount}"
+    IO.puts "And you're tipping out at #{tip_percentage}%"
+    IO.puts "which is: $#{tip_amount}"
+    IO.puts "for a total of: $#{bill_total}"
+  end
 end
--- elixir/01_tip_calc/test/tip_calc_test.exs
+++ elixir/01_tip_calc/test/tip_calc_test.exs
@@ -1,16 +1,12 @@
 defmodule TipCalcTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest TipCalc
 
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-
-  test "find tip amount" do
-    assert TipCalc.calc_tip(20, 25.53) == 5.11
-  end
-
-  test "if decimal tips are just used in calculation instead of being divided" do
-    assert TipCalc.calc_tip(0.15, 25.53) == 3.83
-  end
+  # test "find tip amount" do
+  #   assert TipCalc.calc_tip(20, 25.53) == 5.11
+  # end
+  #
+  # test "if decimal tips are just used in calculation instead of being divided" do
+  #   assert TipCalc.calc_tip(0.15, 25.53) == 3.83
+  # end
 end
