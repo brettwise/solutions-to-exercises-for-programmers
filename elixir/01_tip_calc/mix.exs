8d7c001e3e609a1f95861438f2795f7b91b62b9f
Brett Wise <brettdwise@gmail.com>
2016-07-26 12:52:26 -0400

    Added timex for date/time parsing

--- elixir/01_tip_calc/mix.exs
+++ elixir/01_tip_calc/mix.exs
@@ -15,7 +15,7 @@ defmodule TipCalc.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :timex, :tzdata]]
   end
 
   # Dependencies can be Hex packages:
@@ -28,6 +28,6 @@ defmodule TipCalc.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:timex, "~> 3.0"}]
   end
 end
