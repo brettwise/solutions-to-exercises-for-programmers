ac724d7d4a0daee5edfb2098f6027b49b619d687
Brett Wise <brettdwise@gmail.com>
2016-07-27 00:33:55 -0400

    Made test that tests the test pass the test

--- js/01_tip_calc/test/arrayTest.js
+++ js/01_tip_calc/test/arrayTest.js
@@ -1,9 +1,9 @@
-var assert = chai.assert;
+var assert = require('chai').assert;
 
 describe('Array', function() {
   it('should start empty', function() {
     var arr = [];
 
-    assert.equal(arr.length, 1, 'Array length was not 0');
+    assert.equal(arr.length, 0, 'Array length was not 0');
   });
 });
