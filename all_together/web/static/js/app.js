c44275640dd4fcaf057f2f9c1d2c33c4f46664d3
Brett Wise <brettdwise@gmail.com>
2016-08-02 20:46:04 -0400

    Migrated tip calc from js portion of project into phoenix all together project

--- all_together/web/static/js/app.js
+++ all_together/web/static/js/app.js
@@ -19,3 +19,26 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+
+var calcBillButton = document.getElementById('calc-bill');
+calcBillButton.addEventListener('click', calcTotalWithTip, false);
+
+
+function calcTotalWithTip() {
+
+  var tipPercentage = parseInt(
+    document.getElementById('tip-percentage').value
+    , 10
+  );
+
+  var billAmount = parseFloat(
+    document.getElementById('bill-amount').value
+  );
+
+  var billTotal = document.getElementById('bill-total').innerHTML;
+
+  var tipAmount = tipPercentage/100 * billAmount;
+  var billTotal = tipAmount + billAmount;
+  document.getElementById('bill-total').innerHTML = billTotal.toFixed(2);
+}
